c86d63ab124ea1368eaac0b23a454fcf1841c078
Bastien Nocera <hadess@hadess.net>
2022-09-16 14:53:13 +0200

    Set the default branch to "test" for local builds

--- local-build.sh
+++ local-build.sh
@@ -4,5 +4,7 @@ rm -f com.carpeludum.KegaFusion.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-flatpak-builder --ccache --force-clean _build com.carpeludum.KegaFusion.yml --repo=_repo
-flatpak build-bundle _repo com.carpeludum.KegaFusion.flatpak com.carpeludum.KegaFusion master
+BRANCH=test
+
+flatpak-builder --ccache --force-clean --default-branch=$BRANCH _build com.carpeludum.KegaFusion.yml --repo=_repo
+flatpak build-bundle _repo com.carpeludum.KegaFusion.flatpak com.carpeludum.KegaFusion $BRANCH
